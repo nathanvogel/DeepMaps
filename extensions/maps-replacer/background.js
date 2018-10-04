1ea8cfb9c661d28f85ec8de70feee0c86f202a4a
Nathan Vogel <nathan@nathanvogel.com>
2018-10-04 19:58:37 +0200

    Can pass images

--- extensions/maps-replacer/background.js
+++ extensions/maps-replacer/background.js
@@ -17,37 +17,36 @@ function listener(details) {
   return {};
 }
 
-// eslint-disable-next-line no-unused-vars
+var endpoint = "http://localhost:8080";
+function formatURLParams(params) {
+  return (
+    "?" +
+    Object.keys(params)
+      .map(key => {
+        return key + "=" + encodeURIComponent(params[key]);
+      })
+      .join("&")
+  );
+}
+
 function replaceImage(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
-  console.log("Downloading");
   console.log(details.url);
-  // browser.downloads.download({ url: details.url });
+  console.log(endpoint + formatURLParams([details.url]));
+
   var xhr = new XMLHttpRequest();
-
-  // Use JSFiddle logo as a sample image to avoid complicating
-  // this example with cross-domain issues.
-  xhr.open("GET", "https://localhost:8080/", true);
-
+  xhr.open("GET", endpoint + formatURLParams([details.url]), true);
   // Ask for the result as an ArrayBuffer.
   xhr.responseType = "arraybuffer";
-
   xhr.onload = function(e) {
-    console.log("LOADED");
-    // Obtain a blob: URL for the image data.
-    var arrayBufferView = new ArrayBuffer(this.response);
-    console.log(arrayBufferView);
+    console.log("Loaded custom image");
+    var arrayBufferView = this.response;
 
     filter.ondata = event => {
-      console.log(event.data);
+      // console.log(event.data);
       filter.write(arrayBufferView);
       filter.disconnect();
     };
-    // var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
-    // var urlCreator = window.URL || window.webkitURL;
-    // var imageUrl = urlCreator.createObjectURL( blob );
-    // var img = document.querySelector( "#photo" );
-    // img.src = imageUrl;
   };
 
   xhr.send();
