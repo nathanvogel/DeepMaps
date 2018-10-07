11d3460958399150e85e4101f6405f2f5d9ae754
Nathan Vogel <nathan@nathanvogel.com>
2018-10-07 04:24:18 +0200

    :bug: Fix handling of all async cases in the extension

--- extensions/maps-replacer/background.js
+++ extensions/maps-replacer/background.js
@@ -29,50 +29,45 @@ function formatURLParams(params) {
   );
 }
 
-var last
-
 function replaceImage(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
-  console.log(details.url);
-  console.log(endpoint + formatURLParams([details.url]));
+  // console.log(details.url);
+  // console.log(endpoint + formatURLParams([details.url]));
+
+  // The URL to query our local server:
   let aiUrl = endpoint + formatURLParams({ originalUrl: details.url });
+  // Uncomment to query a dummy image:
   // aiUrl = "https://www.bing.com/sa/simg/hpc26.png";
-  // aiUrl = "http://www.blended.study/uploads/1/2/8/7/12875823/frame-1_orig.png"
-  // aiUrl = "https://upload.wikimedia.org/wikipedia/commons/3/3f/Fronalpstock_big.jpg?test=" + Math.random()
 
-
-  // filter.ondata = event => {
-  //   writeBufferIfAvailable();
-  // };
+  var didReceiveOriginalRequest = false;
+  filter.ondata = event => {
+    // Indicate that we already received the original image.
+    didReceiveOriginalRequest = true;
+  };
 
   var xhr = new XMLHttpRequest();
   xhr.open("GET", aiUrl, true);
   // Ask for the result as an ArrayBuffer.
   xhr.responseType = "arraybuffer";
-  xhr.onload = function(e) {
-    console.log("Loaded custom image");
+  // The the callback for our custom image.
+  xhr.onload = function(event) {
     var buffer = this.response;
-    console.log(this);
-    // var arrayBufferView = new Uint8Array( this.response );
-    // console.log(arrayBufferView);
 
-
-
-    // var blob = new Blob( [ arrayBufferView ], { type: "image/png" } );
-    // var urlCreator = window.URL || window.webkitURL;
-    // var imageUrl = urlCreator.createObjectURL( blob );
-    // console.log(imageUrl);
-
-    // var img = document.getElementsByClassName("button");
-    // img.src = imageUrl;
-
-      console.log("timeout");
-    // filter.ondata = event => {
-    console.log("ONDATA");
-        // console.log(event.data);
-          filter.write(buffer);
-          filter.disconnect();
-      // };
+    if (didReceiveOriginalRequest) {
+      // The filter stream is already open since the original image from
+      // OpenStreetMap loaded faster, so we can directly write to it.
+      console.log("Received generated image after the original image.");
+      filter.write(buffer);
+      filter.disconnect();
+    } else {
+      // The original request hasn't completed yet, so we replace the callback:
+      filter.ondata = event => {
+        console.log("OSM request complete. We can now write our custom image.");
+        filter.write(buffer);
+        filter.disconnect();
+      };
+      console.log("Received generated image before the original image.");
+    }
   };
 
   xhr.send();
